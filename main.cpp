ff58e8d92127520f9f591ef351d8f3995569c01f
Danil Karpenko <limpix31@gmail.com>
2024-11-20 11:31:22 +0100

    optimize cpp sim code

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ typedef uint64_t u64;
 typedef uint32_t u32;
 
 struct Ticker {
+private:
 	Vtop &top;
 	VerilatedVcdC &tfp;
 	u64 cycle = 0;
 
+public:
 	Ticker(Vtop &top, VerilatedVcdC &tfp) : top(top), tfp(tfp) {
 	}
 
@@ -18,42 +20,51 @@ struct Ticker {
 		tfp.dump(cycle);
 		cycle += 1;
 	}
+
+	void tick_with(const std::function<void()> &eval) {
+		eval();
+		tick();
+	}
+
+	void tick_posedge(CData &wire) {
+		tick_with([&] { wire = 1; });
+		tick_with([&] { wire = 0; });
+	}
+
+	void tick_wire(CData &wire, const CData value) {
+		tick_with([&] { wire = value; });
+	}
 };
 
 int main(const int argc, char **argv) {
-	auto *const ctx = new VerilatedContext;
+	const auto ctx = std::make_unique<VerilatedContext>();
 	ctx->commandArgs(argc, argv);
-	auto *const top = new Vtop{ctx};
+	const auto top = std::make_unique<Vtop>(ctx.get());
 
 	Verilated::traceEverOn(true);
-	auto *const tfp = new VerilatedVcdC;
-	top->trace(tfp, 99);
-	tfp->open("wave.vcd");
+	const auto tracer = std::make_unique<VerilatedVcdC>();
+	top->trace(tracer.get(), 99);
+	tracer->open("wave.vcd");
 
-	auto ticker = Ticker(*top, *tfp);
+	auto ticker = Ticker(*top, *tracer);
 
-	#define tick(with) {{with;} ticker.tick();}
-
-	tick(
+	ticker.tick_with([&] {
 		top->clk = 0;
 		top->rst = 1;
-	);
+	});
 
-	tick(top->rst = 0);
+	ticker.tick_wire(top->rst, 0);
 
-	for (u32 count_cycles = 0; count_cycles < 3; count_cycles++) {
-		for (u32 local_cycle = 0; local_cycle < (16 - 1) * 2; ++local_cycle) {
-			tick(top->clk = 1);
-			tick(top->clk = 0);
+	for (u32 count_cycle = 0; count_cycle < 3; ++count_cycle) {
+		for (u32 local_cycle = 0; local_cycle < 15; ++local_cycle) {
+			ticker.tick_posedge(top->clk);
 		}
 
-		tick(top->rst = 1);
-		tick(top->rst = 0);
+		ticker.tick_posedge(top->rst);
 	}
 
-	tfp->close();
+	tracer->close();
 	top->final();
-	delete top;
 
 	return 0;
 }
